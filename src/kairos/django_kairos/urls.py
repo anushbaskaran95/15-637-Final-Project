b7c99200ad2d4d01c94bd5955248c0d9273115b8
Advait Burde <advaitburde@gmail.com>
2017-10-21 16:44:51 -0400

    created views for registration in separate file

--- src/kairos/django_kairos/urls.py
+++ src/kairos/django_kairos/urls.py
@@ -1,16 +1,11 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from . import views as kairos_views
+from views.registration_views import *
 
 urlpatterns = [
-    url(r'^$', kairos_views.dashboard, name='dash'),
-
-    url(r'^login/$', kairos_views.login, name='login'),
-    url(r'^logout$', kairos_views.logout_user, name='logout'),
-
-    url(r'^register/$', kairos_views.register, name='register'),
+    url(r'^register/$', register, name='register'),
     url(r'^confirm-registration/(?P<user_id>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        kairos_views.confirm_registration, name='confirm-registration'),
+        confirm_registration, name='confirm-registration'),
     url(r'^password_reset/$', auth_views.password_reset, {'template_name': 'registration/confirm_email.html'},
         name='password_reset'),
     url(r'^password_reset/done/$', auth_views.password_reset_done, {'template_name': 'registration/email_sent.html'},
@@ -22,7 +17,5 @@ urlpatterns = [
     url(r'^reset/done/$', auth_views.password_reset_complete,
         {'template_name': 'registration/password_reset_success.html'},
         name='password_reset_complete'),
-    url(r'^change-password$', kairos_views.change_password, name='change-password'),
-
-    url(r'^(?P<user_id>\d+)/edit-profile/$', kairos_views.edit_profile, name='edit-profile'),
+    url(r'^change-password$', change_password, name='change-password'),
 ]
