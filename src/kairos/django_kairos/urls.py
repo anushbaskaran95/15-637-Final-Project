3352e6bd39127b7a59685e0cc91e2bc69731b74f
anushbaskaran95 <anushbaskaran1995@gmail.com>
2017-10-22 14:03:00 -0400

    Design landing page for Kairos

--- src/kairos/django_kairos/urls.py
+++ src/kairos/django_kairos/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from views import *
+from views.registration_views import *
+from views.dashboard_views import *
+from views.login import *
 
 urlpatterns = [
     url(r'^$', home, name='home'),
