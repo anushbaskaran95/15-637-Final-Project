2f99a3c9597d146bc8ef9f4c46783975cb2791f5
anushbaskaran95 <anushbaskaran1995@gmail.com>
2017-10-27 09:58:38 -0400

    Add forgot password

--- src/kairos/django_kairos/urls.py
+++ src/kairos/django_kairos/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 from views.registration_views import *
 from views.dashboard_views import *
 from views.login import *
@@ -8,15 +8,14 @@ urlpatterns = [
     url(r'^register/$', register, name='register'),
     url(r'^confirm-registration/(?P<user_id>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         confirm_registration, name='confirm-registration'),
-    url(r'^password_reset/$', auth_views.password_reset, {'template_name': 'registration/confirm_email.html'},
-        name='password_reset'),
-    url(r'^password_reset/done/$', auth_views.password_reset_done, {'template_name': 'registration/email_sent.html'},
+    url(r'^password_reset/$', password_reset, {'template_name': 'registration/confirm_email.html', 'post_reset_redirect': 'password_reset_done'}, name='password_reset'),
+    url(r'^password_reset/done/$', password_reset_done, {'template_name': 'registration/email_sent.html'},
         name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm,
-        {'template_name': 'registration/password_reset.html'},
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        password_reset_confirm,
+        {'template_name': 'registration/password_reset.html', 'post_reset_redirect': 'password_reset_complete'},
         name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete,
+    url(r'^reset/done/$', password_reset_complete,
         {'template_name': 'registration/password_reset_success.html'},
         name='password_reset_complete'),
     url(r'^change-password$', change_password, name='change-password'),
