28a95d3f5f9d739690d7536cd728138511c11b3a
Advait Burde <advaitburde@gmail.com>
2017-11-18 12:53:01 -0500

    UI changes on task cards

--- src/kairos/django_kairos/static/js/task_button.js
+++ src/kairos/django_kairos/static/js/task_button.js
@@ -1,50 +1,42 @@
 $( document ).ready(function() {  // Runs when the document is ready
-  /**
-   * the current path
-   *
-   */
-  //var pathname = window.location.pathname; // Returns path only
-  /**
-   * once user has clicked the switch, it will send the boolean value and corresponding
-   * task id to the backend for further processing.
-   * Info is to tell backend where this infomation comes from.
-   *
-   */
-  $(".switch").find("input[type=checkbox]").on("change",function() {
-       var status = $(this).prop('checked');
-       var task_id = $(this).parent().next().val();
-       var info = "switch";
-       console.log(status);
-       console.log(task_id);
-       //console.log(info);
-       //console.log(pathname);
+   /**
+    * once user has clicked the switch, it will send the boolean value and corresponding
+    * task id to the backend for further processing.
+    * Info is to tell backend where this infomation comes from.
+    *
+    */
+    $(".switch").find("input[type=checkbox]").on("change",function() {
+        var status = $(this).prop('checked');
+        var task_id = $(this).val();
+        var info = "switch";
+        //console.log(status);
+        //console.log(task_id);
         $.ajax({
-           url : 'button_process',
-           type : "post",
-           data : {
-             status : status,
-             task_id : task_id,
-             info : info
+            url : 'button_process',
+            type : "post",
+            data : {
+                status : status,
+                task_id : task_id,
+                info : info
            }
        });
-   });
-   /**
-    * once user has clicked any of stop button, it will send its id to backend.
-    * Info is to tell backend where this infomation comes from.
-    */
-  $(".stop").click(function(){
-    var task_id = $(this).parent().prev().find('input[name = id]').val();
-    var info = "stop button";
-    // console.log(task_id);
-    // console.log(info);
-    $.ajax({
-       url : 'stop_process',
-       type : "post",
-       data : {
-         task_id : task_id,
-         info : info
-       }
-   });
-  });
+    });
+    /**
+     * once user has clicked any of stop button, it will send its id to backend.
+     * Info is to tell backend where this infomation comes from.
+     */
+    $(".stop").click(function(){
+        var task_id = $(this).attr('id');
+        var info = "stop button";
+        //console.log(task_id);
+        $.ajax({
+            url : 'stop_process',
+            type : "post",
+            data : {
+                task_id : task_id,
+                info : info
+            }
+        });
+    });
 
 }); // End of $(document).ready
