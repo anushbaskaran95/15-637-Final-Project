75721bc1dc2204fde5b1bb6748a78be509b9ed9c
Advait Burde <advaitburde@gmail.com>
2017-11-25 19:32:17 -0500

    fixed css issue and future start time message

--- src/kairos/django_kairos/templatetags/class_tag.py
+++ src/kairos/django_kairos/templatetags/class_tag.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter(name='get_class')
 def get_class(value):
     return value.__class__.__name__
+
+
+@register.filter
+def calc_time(value):
+    return "{0:.1f}".format(value / 3600.0)
