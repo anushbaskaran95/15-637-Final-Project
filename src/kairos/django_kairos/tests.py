9cf25ac9ea504045222bd2c7ea8d80e82599517b
Advait Burde <advaitburde@gmail.com>
2017-11-30 18:40:33 -0500

    merging

--- src/kairos/django_kairos/tests.py
+++ src/kairos/django_kairos/tests.py
@@ -20,8 +20,13 @@ class TestUrls(TestCase):
 
     def test_dash(self):
         client = Client()
+        user = User.objects.create(username='testuser')
+        user.set_password('password')
+        user.save()
+
+        client.login(username='testuser', password='password')
         response = client.get('')
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
 
     def test_coursework(self):
         client = Client()
@@ -30,7 +35,17 @@ class TestUrls(TestCase):
 
     def test_get_task_info(self):
         client = Client()
+        user = User.objects.create(username='testuser')
+        user.set_password('password')
+        user.save()
+
+        client.login(username='testuser', password='password')
         response = client.get('/get-task-info')
+        self.assertEqual(response.status_code, 200)
+
+    def test_analytics(self):
+        client = Client()
+        response = client.get('/analytics')
         self.assertEqual(response.status_code, 302)
 
 
@@ -38,5 +53,19 @@ class TestModels(TestCase):
 
     fixtures = ['db']
 
-    # def create_course(self):
-    #     return
+    def test_create_course(self):
+        client = Client()
+        user = User.objects.create(username='testuser')
+        user.set_password('password')
+        user.save()
+
+        client.login(username='testuser', password='password')
+
+        course = Course(user=user, course_name='Test Course')
+        course.save()
+
+        self.assertTrue(len(Course.objects.filter(user=user,
+                                                  course_name__iexact='Test Course')) == 1)
+
+        self.assertTrue(len(Course.objects.filter(user=user,
+                                                  course_name__iexact='test course')) == 1)
